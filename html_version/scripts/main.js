cf3a920604016fb4c29fe1b0e531d41ed594fc8d
Justin <goshornjustin@gmail.com>
2025-06-12 15:50:30 -0400

    added createCard html page, other changes

--- html_version/scripts/main.js
+++ html_version/scripts/main.js
@@ -1,20 +1,19 @@
-const duck = '/assets';
 
-const sampleDeck = [
+let sampleDeck = [
   {
     title: "The Rubber Duck",
     message: "Explain your bug to an inanimate object. It will solve itself.",
-    image: duck,
+    image: '/assets/images/rubber-duck.jpg',
   },
   {
     title: "The Refactor",
     message: "You are tempted to rewrite. Sometimes evolution > revolution.",
-    image: evRImage,
+    image: "/assets/images/ev-rev.jpg",
   },
   {
     title: "The Commit",
     message: "Commit messages are like love letters to your future self.",
-    image: love,
+    image: '/assets/images/love-letter.jpg',
   },
 ];
 
@@ -24,9 +23,27 @@ const sampleDeck = [
     image:'',
   }
 
-  function drawCard(card) {
+  function drawCard() {
     const random = sampleDeck[Math.floor(Math.random() * sampleDeck.length)];
-    return random;
+    console.log(random);
+
+    const title = document.getElementById("card-title");
+    const message = document.getElementById("card-message");
+    const image = document.querySelector('.image');
+
+
+    title.innerHTML = random.title;
+    message.innerHTML = random.message;
+    if (image instanceof HTMLImageElement) {
+       image.src = random.image;
+    }
+
+    removeFromList(random);
+   
   }
 
-  
+  function removeFromList(random) {
+   sampleDeck = sampleDeck.filter(e => e.title !== random.title);
+    console.log(`${random} removed from list`);
+    console.log(sampleDeck);
+  }
